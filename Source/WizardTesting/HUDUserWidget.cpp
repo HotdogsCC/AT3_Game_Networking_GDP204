9cf756e4c4a1662546aafef56c8a881e4b6dff5d
Charlie Campbell <chacha9669@hotmail.com>
2025-11-07 13:35:01 +1100

    comments

--- Source/WizardTesting/HUDUserWidget.cpp
+++ Source/WizardTesting/HUDUserWidget.cpp
@@ -12,57 +12,71 @@ void UHUDUserWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 
+	//get a reference to the owning player
 	Player = Cast<AWizardCharacter>(GetOwningPlayer()->GetPawn());
 	checkf(Player, TEXT("Failed to get reference to owning Player"));
 
+	//cast the hand canvas to a slot
 	HandCanvasAsSlot = Cast<UCanvasPanelSlot>(HandCanvas->Slot);
 	checkf(HandCanvasAsSlot, TEXT("Failed to cast HandCanvasSlot"));
 }
 
 void UHUDUserWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 {
+	//bob the head
 	HeadBob(Player->GetPlayerSpeed());
 }
 
 
 void UHUDUserWidget::HeadBob(float PlayerSpeed)
 {
+	//get time in seconds
 	const float Time = UGameplayStatics::GetTimeSeconds(this);
-	
+
+	//clamp speed (so it is always bobbing)
 	constexpr float MinPlayerSpeed = 120.0f;
 	constexpr float MaxPlayerSpeed = 100000.0f;
 	const float ClampedPlayerSpeed = FMath::Clamp(PlayerSpeed, MinPlayerSpeed, MaxPlayerSpeed);
 
+	//set the speed and size of the bob
 	constexpr float HeadBobSpeed = 10.0f;
 	constexpr float HeadBobSizeScale = 0.02f;
 
+	//calculate sin for this frame
 	float Offset = FMath::Sin(HeadBobSpeed * Time);
 
+	//transform sin between 0 and 1
 	Offset += 1.0f;
 	Offset /= 2.0f;
 
+	//transform by size and speed
 	Offset *= ClampedPlayerSpeed;
 	Offset *= HeadBobSizeScale;
-	
+
+	//apply transformation
 	HandCanvasAsSlot->SetPosition(FVector2D(0.0f, Offset));
 	
 }
 
 void UHUDUserWidget::CanFire() const
 {
+	//make the ui show an image of a hand with fire
 	LeftHand->SetBrushFromTexture(HandWithFire);
 }
 
 void UHUDUserWidget::CannotFire()
 {
+	//make the ui show the hand casting the fire
 	LeftHand->SetBrushFromTexture(HandCastingFire);
 
+	//make it show an empty hand in half a second
 	GetWorld()->GetTimerManager().SetTimer(FireCastingTimerHandle, this,
 		&UHUDUserWidget::FinishCasting, 0.5f, false);
 }
 
 void UHUDUserWidget::FinishCasting() const
 {
+	//make the ui show an empty hand
 	LeftHand->SetBrushFromTexture(HandWithNoFire);
 }
 
--- Source/WizardTesting/HUDUserWidget.h
+++ Source/WizardTesting/HUDUserWidget.h
@@ -18,45 +18,59 @@ class WIZARDTESTING_API UHUDUserWidget : public UUserWidget
 {
 	GENERATED_BODY()
 public:
+	//when the widget is created
 	virtual void NativeConstruct() override;
 
+	//every update
 	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
-	
+
+	//when the player can no longer fire
 	UFUNCTION()
 	void CannotFire();
 
+	//when the player is able to fire
 	UFUNCTION()
 	void CanFire() const;
 
+	//makes the hud bob up and down based on player speed
 	UFUNCTION()
 	void HeadBob(float PlayerSpeed);
 
 protected:
+	//the canvas containing the hands
 	UPROPERTY(meta=(BindWidget))
 	UCanvasPanel* HandCanvas = nullptr;
 
+	//the image slot for the left hand
 	UPROPERTY(meta=(BindWidget))
 	UImage* LeftHand = nullptr;
 
+	//the texture of the hand holding fire
 	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess="true"))
 	UTexture2D* HandWithFire;
 
+	//the texture of the hand holding nothing
 	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess="true"))
 	UTexture2D* HandWithNoFire;
 
+	//the texture of the hand casting the magic
 	UPROPERTY(EditAnywhere, meta=(AllowPrivateAccess="true"))
 	UTexture2D* HandCastingFire;
 
 private:
+	//called shortly after CannotFire; makes hud transition from casting to holding nothing
 	UFUNCTION()
 	void FinishCasting() const;
-	
+
+	//reference to the player
 	UPROPERTY()
 	AWizardCharacter* Player = nullptr;
 
+	//the canvas with the hands as a canvas slot; needed for transformation
 	UPROPERTY()
 	UCanvasPanelSlot* HandCanvasAsSlot = nullptr;
 
+	//a handle for the delay between CannotFire() and FinishCasting()
 	UPROPERTY()
 	FTimerHandle FireCastingTimerHandle;
 };
